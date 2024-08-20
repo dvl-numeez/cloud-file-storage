362e6106ee0d73ccbd40cb5f87c1b806a610ceb9
numeezbaloch17 <numeezbaloch17@gmail.com>
2024-08-20 13:46:37 +0530

    final code

--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
-import (
-	"context"
-	"log"
-)
 
 
 
 
 func main()  {
-	client,err:=NewMinioClient()
-	if err!=nil{
-		log.Fatal("Error occured while connecting to minio : ",err)
-	}
-	err=client.deleteFile(context.Background(),"cloud-fs","go-info")
-	if err!=nil{
-		log.Fatal("error occured file deleting a file : ",err)
-	}
-	
+
 }
